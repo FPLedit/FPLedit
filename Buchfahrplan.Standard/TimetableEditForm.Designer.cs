1dea29298e0710bd9b0922dcbd55c3972c7fe89c
Manuel Huber <ich@manuelhu.de>
2016-10-02 17:49:59 +0200

    TimetableEditScreen: Resizable window

--- Buchfahrplan.Standard/TimetableEditForm.Designer.cs
+++ Buchfahrplan.Standard/TimetableEditForm.Designer.cs
@@ -28,36 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.topDataGridView = new System.Windows.Forms.DataGridView();
             this.cancelButton = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Button();
-            this.bottomDataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.topFromToLabel = new System.Windows.Forms.Label();
+            this.topDataGridView = new System.Windows.Forms.DataGridView();
             this.bottomFromToLabel = new System.Windows.Forms.Label();
+            this.bottomDataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.topDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bottomDataGridView)).BeginInit();
             this.SuspendLayout();
-            // 
-            // topDataGridView
-            // 
-            this.topDataGridView.AllowUserToAddRows = false;
-            this.topDataGridView.AllowUserToDeleteRows = false;
-            this.topDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
-            this.topDataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.topDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.topDataGridView.Location = new System.Drawing.Point(12, 33);
-            this.topDataGridView.MultiSelect = false;
-            this.topDataGridView.Name = "topDataGridView";
-            this.topDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.topDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.topDataGridView.Size = new System.Drawing.Size(956, 271);
-            this.topDataGridView.TabIndex = 0;
             // 
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(810, 627);
+            this.cancelButton.Location = new System.Drawing.Point(683, 603);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 24;
@@ -69,7 +60,7 @@
             // 
             this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.closeButton.Location = new System.Drawing.Point(891, 627);
+            this.closeButton.Location = new System.Drawing.Point(763, 603);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(75, 23);
             this.closeButton.TabIndex = 23;
@@ -77,42 +68,104 @@
             this.closeButton.UseVisualStyleBackColor = true;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
-            // bottomDataGridView
+            // tableLayoutPanel1
             // 
-            this.bottomDataGridView.AllowUserToAddRows = false;
-            this.bottomDataGridView.AllowUserToDeleteRows = false;
-            this.bottomDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
-            this.bottomDataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.bottomDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.bottomDataGridView.Location = new System.Drawing.Point(12, 339);
-            this.bottomDataGridView.MultiSelect = false;
-            this.bottomDataGridView.Name = "bottomDataGridView";
-            this.bottomDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.bottomDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.bottomDataGridView.Size = new System.Drawing.Size(956, 271);
-            this.bottomDataGridView.TabIndex = 25;
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(826, 585);
+            this.tableLayoutPanel1.TabIndex = 29;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.topFromToLabel);
+            this.panel1.Controls.Add(this.topDataGridView);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.bottomFromToLabel);
+            this.panel2.Controls.Add(this.bottomDataGridView);
+            this.panel2.Location = new System.Drawing.Point(3, 295);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(820, 287);
+            this.panel2.TabIndex = 1;
             // 
             // topFromToLabel
             // 
             this.topFromToLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.topFromToLabel.Location = new System.Drawing.Point(9, 9);
+            this.topFromToLabel.Location = new System.Drawing.Point(3, 0);
             this.topFromToLabel.Name = "topFromToLabel";
-            this.topFromToLabel.Size = new System.Drawing.Size(955, 15);
-            this.topFromToLabel.TabIndex = 27;
+            this.topFromToLabel.Size = new System.Drawing.Size(814, 15);
+            this.topFromToLabel.TabIndex = 33;
             this.topFromToLabel.Text = "Züge von ... nach ...";
             this.topFromToLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topDataGridView
+            // 
+            this.topDataGridView.AllowUserToAddRows = false;
+            this.topDataGridView.AllowUserToDeleteRows = false;
+            this.topDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.topDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.topDataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.topDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.topDataGridView.Location = new System.Drawing.Point(3, 18);
+            this.topDataGridView.MultiSelect = false;
+            this.topDataGridView.Name = "topDataGridView";
+            this.topDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.topDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.topDataGridView.Size = new System.Drawing.Size(814, 268);
+            this.topDataGridView.TabIndex = 32;
             // 
             // bottomFromToLabel
             // 
             this.bottomFromToLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.bottomFromToLabel.Location = new System.Drawing.Point(9, 321);
+            this.bottomFromToLabel.Location = new System.Drawing.Point(3, 0);
             this.bottomFromToLabel.Name = "bottomFromToLabel";
-            this.bottomFromToLabel.Size = new System.Drawing.Size(955, 15);
-            this.bottomFromToLabel.TabIndex = 28;
+            this.bottomFromToLabel.Size = new System.Drawing.Size(814, 15);
+            this.bottomFromToLabel.TabIndex = 30;
             this.bottomFromToLabel.Text = "Züge von ... nach ...";
             this.bottomFromToLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomDataGridView
+            // 
+            this.bottomDataGridView.AllowUserToAddRows = false;
+            this.bottomDataGridView.AllowUserToDeleteRows = false;
+            this.bottomDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bottomDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.bottomDataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.bottomDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bottomDataGridView.Location = new System.Drawing.Point(0, 18);
+            this.bottomDataGridView.MultiSelect = false;
+            this.bottomDataGridView.Name = "bottomDataGridView";
+            this.bottomDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.bottomDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.bottomDataGridView.Size = new System.Drawing.Size(820, 266);
+            this.bottomDataGridView.TabIndex = 29;
             // 
             // TimetableEditForm
             // 
@@ -120,16 +173,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(985, 662);
-            this.Controls.Add(this.bottomFromToLabel);
-            this.Controls.Add(this.topFromToLabel);
-            this.Controls.Add(this.bottomDataGridView);
+            this.ClientSize = new System.Drawing.Size(850, 638);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.closeButton);
-            this.Controls.Add(this.topDataGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "TimetableEditForm";
             this.Text = "Fahrplan bearbeiten";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.topDataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bottomDataGridView)).EndInit();
             this.ResumeLayout(false);
@@ -137,12 +190,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView topDataGridView;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button closeButton;
-        private System.Windows.Forms.DataGridView bottomDataGridView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label topFromToLabel;
+        private System.Windows.Forms.DataGridView topDataGridView;
         private System.Windows.Forms.Label bottomFromToLabel;
+        private System.Windows.Forms.DataGridView bottomDataGridView;
     }
 }
