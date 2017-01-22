c4bbe8ab62aa068ba779e4f64798ae709b0a0691
Manuel Huber <ich@manuelhu.de>
2017-01-22 18:57:27 +0100

    Remove unsused plugins from app.config

--- FPLedit.Standard/Plugin.cs
+++ FPLedit.Standard/Plugin.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace FPLedit.Standard
 {
-    public class EditorPlugin : IPlugin
+    public class Plugin : IPlugin
     {
         public string Name
         {
