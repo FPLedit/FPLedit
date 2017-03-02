69f914fdf9b48b805212e2317c9f520588df78db
Manuel Huber <ich@manuelhu.de>
2017-03-02 16:05:22 +0100

    Add simple installer

---
+++ FPLedit.Installer/Installer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FPLedit.Installer
+{
+    internal class Installer
+    {
+        public void Install()
+        {
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                MessageBox.Show("This installer works only on Windows!");
+                return;
+            }
+
+            if (MessageBox.Show("Would you like FPLedit as file handler for .fpl?", "FPLedit Installer", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            RegistryKey ckey = Registry
+                .ClassesRoot
+                .CreateSubKey("fpledit_fpl_file")
+                .CreateSubKey("shell")
+                .CreateSubKey("open")
+                .CreateSubKey("command");
+
+            ckey.SetValue("", "\"F:\\VS-Projects\\Buchfahrplan\\Buchfahrplan\\bin\\Debug\\FPLedit.exe\" \"%1\"");
+            ckey.Close();
+
+            RegistryKey fkey = Registry
+                .ClassesRoot
+                .CreateSubKey(".fpl");
+            fkey.SetValue("", "fpledit_fpl_file");
+            fkey.Close();
+
+            MessageBox.Show("Installed succesfully!");
+        }
+    }
+}
---
+++ FPLedit.Installer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FPLedit.Installer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            (new Installer()).Install();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
