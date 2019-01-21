018274792140c9596386710d8851f8975e420b73
Manuel Huber <ich@manuelhu.de>
2019-01-21 21:55:25 +0100

    Add DebugCump project to gather debug data

---
+++ FPLedit.DebugDump/FileLogger.cs
@@ -0,0 +1,78 @@
+﻿using FPLedit.Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FPLedit.DebugDump
+{
+    public class FileLogger : ILog
+    {
+        private string filename;
+
+        public FileLogger(IInfo info, string fn)
+        {
+            filename = fn;
+
+            if (File.Exists(filename) && new FileInfo(filename).Length > 10240) // > 10KB
+                File.Delete(filename);
+
+            Write("FPLedit Programmstart", "INFO");
+        }
+
+        public void Error(string message)
+        {
+            Write(message, "EROR");
+        }
+
+        public void Info(string message)
+        {
+            Write(message, "INFO");
+        }
+
+        public void LogException(Exception e)
+        {
+            string details = "Fehler beim Erstellen der Fehlerinformationen";
+            try
+            {
+                details = GetExceptionDetails(e);
+            }
+            catch { }
+            Write(details, "EXCP");
+        }
+
+        private string GetExceptionDetails(Exception exception)
+        {
+            var properties = exception.GetType().GetProperties();
+            var fields = properties.Select(property => new {
+                property.Name,
+                Value = property.GetValue(exception, null)
+            })
+                .Select(x => string.Format(
+                    "{0} = {1}",
+                    x.Name,
+                    x.Value != null ? x.Value.ToString() : ""
+                ));
+            return string.Join("\n", fields);
+        }
+
+        public void Warning(string message)
+        {
+            Write(message, "WARN");
+        }
+
+        public void Debug(string message)
+        {
+            Write(message, "DEBUG");
+        }
+
+        private void Write(string message, string type)
+        {
+            using (StreamWriter r = new StreamWriter(filename, true))
+            {
+                r.WriteLine(DateTime.Now.ToString() + ": [" + type + "] " + message);
+            }
+        }
+    }
+}
---
+++ FPLedit.DebugDump/Plugin.cs
@@ -0,0 +1,113 @@
+﻿using FPLedit.Shared;
+using FPLedit.Shared.Filetypes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPLedit.DebugDump
+{
+    [Plugin("DebugDump", Vi.PFrom, Vi.PUpTo, Author = "Manuel Huber")]
+    public class Plugin : IPlugin
+    {
+        private string basePath;
+        private DirectoryInfo dir;
+        private int sessionCounter = 0;
+        private string session;
+        private IInfo info;
+
+        public void Init(IInfo info)
+        {
+            XMLEntity last = new XMLEntity("dummy");
+
+            this.info = info;
+
+            session = Guid.NewGuid().ToString();
+            basePath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "ttDumps", session);
+            dir = new DirectoryInfo(basePath);
+
+            if (!dir.Exists)
+                dir.Create();
+
+            var logPath = Path.Combine(basePath, "session.log");
+            dynamic l = info.Logger;
+            l.Loggers.Add(new FileLogger(info, logPath));
+
+            info.FileStateChanged += (s, e) =>
+            {
+                try
+                {
+                    if (info.Timetable == null || XDiff(last, info.Timetable.XMLEntity))
+                        return;
+
+                    var clone = info.Timetable.Clone();
+                    var exp = new XMLExport();
+
+                    var fn = Path.Combine(basePath, $"fpldump-{sessionCounter++}.fpl");
+
+                    var x = new XMLEntity("dump-info");
+                    x.SetAttribute("session", session);
+                    x.SetAttribute("date", DateTime.Now.ToShortDateString());
+                    x.SetAttribute("time", DateTime.Now.ToShortTimeString());
+                    x.SetAttribute("pf", Environment.OSVersion.Platform.ToString());
+
+                    clone.Children.Add(x);
+                    exp.Export(clone, fn, info);
+                    info.Logger.Debug("Dump erstellt: " + fn);
+
+                    var clone2 = info.Timetable.Clone();
+                    last = clone2.XMLEntity;
+                }
+                catch { }
+            };
+
+            info.ExtensionsLoaded += (s, e) =>
+            {
+                info.Logger.Info("DebugDump aktiviert. Session: " + session);
+            };
+
+            var tmpDir = info.GetTemp("");
+            var watcher = new FileSystemWatcher(tmpDir, "*.*");
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += WatcherEvent;
+            watcher.Created += WatcherEvent;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void WatcherEvent(object sender, FileSystemEventArgs e)
+        {
+            var orig = new FileInfo(e.FullPath);
+            if (!orig.Exists)
+                return;
+            var fn = Path.Combine(basePath, $"tmpdump-{sessionCounter++}-{Path.GetFileNameWithoutExtension(orig.Name)}{orig.Extension}");
+            info.Logger.Debug("Dump erstellt: " + fn);
+            orig.CopyTo(fn);
+        }
+
+        private bool XDiff(XMLEntity x1, XMLEntity x2)
+        {
+            if (x1.XName != x2.XName)
+                return false;
+            if (x1.Value != x2.Value)
+                return false;
+            if (x1.Attributes.Count != x2.Attributes.Count)
+                return false;
+            foreach (var a in x1.Attributes)
+            {
+                if (a.Value != x2.GetAttribute<string>(a.Key, null))
+                    return false;
+            }
+            if (x1.Children.Count != x2.Children.Count)
+                return false;
+            foreach (var c in x1.Children)
+            {
+                if (!XDiff(c, x2.Children[x1.Children.IndexOf(c)]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
