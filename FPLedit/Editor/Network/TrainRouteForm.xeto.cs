aea9b6074a6674f62d450a8e780e821df90fec70
Manuel Huber <ich@manuelhu.de>
2019-07-27 13:59:03 +0200

    Refactor train path editor

--- FPLedit/Editor/Network/TrainRouteForm.xeto.cs
+++ FPLedit/Editor/Network/TrainRouteForm.xeto.cs
@@ -8,7 +8,6 @@ using System.Linq;
 
 namespace FPLedit.Editor.Network
 {
-    //TODO: Refactor, better state handling (simple state machine, enum, attributes for status string?)
     internal class TrainRouteForm : FDialog<DialogResult>
     {
 #pragma warning disable CS0649
@@ -18,106 +17,121 @@ namespace FPLedit.Editor.Network
         private readonly UrlButton waypointsDocuLink;
 #pragma warning restore CS0649
 
-        private readonly IInfo info;
+        private readonly Pathfinder pathfinder;
+        private readonly bool globalWaypointsAllowed = false;
 
-        public List<Station> Path { get; private set; }
-        private Train train;
-
-        private Station staStart, staEnd; // Set-Mode
-
-        private bool waypointsAllowed = false;
+        // Internal state: path & waypoints
         private readonly List<Station> wayPoints = new List<Station>();
+        private List<Station> path;
 
-        private TrainRouteForm(IInfo info)
+        public List<Station> Path
+        {
+            get => path; private set
+            {
+                path = value;
+                lineRenderer.SetHighlightedPath(value);
+            }
+        }
+
+        private readonly Train train;
+
+        private readonly RouteEditState stateSetRoute, stateChangeRoute, stateAddWaypoints;
+
+        private TrainRouteForm(IInfo info, bool waypointsAllowed, Train train = null)
         {
             Eto.Serialization.Xaml.XamlReader.Load(this);
 
-            this.info = info;
+            pathfinder = new Pathfinder(info.Timetable);
 
             lineRenderer.StationMovingEnabled = false;
             lineRenderer.HighlightBetweenStations = true;
             lineRenderer.SelectedRoute = -1;
             lineRenderer.SetTimetable(info.Timetable);
             lineRenderer.DisableTopBorder = true;
+            lineRenderer.StationClicked += HandleStationClick;
+
+            globalWaypointsAllowed =
+                waypointsCheckBox.Visible =
+                waypointsDocuLink.Visible = waypointsAllowed;
+
+            // Maybe initialize train data
+            this.train = train;
+            if (train != null)
+                Path = train.GetPath();
+
+            // Initialize edit states
+            stateSetRoute = new RouteEditState()
+            {
+                InitialStatusString = "Startstation auswählen",
+                StationClicked = SetRoute,
+                WaypointsTransitionAllowed = false, // Waypoints can be edited only after creating initial route
+                IsTerminating = false,
+                StateInitialize = InitSetRoute,
+            };
+
+            stateChangeRoute = new RouteEditState()
+            {
+                InitialStatusString = "Durch Klick Stationen am Anfang/Ende des Laufwegs entfernen",
+                StationClicked = ChangeRoute,
+                WaypointsTransitionAllowed = true, // Set with global values
+                IsTerminating = true,
+            };
+
+            stateAddWaypoints = new RouteEditState()
+            {
+                InitialStatusString = "Durch Klick Wegpunkte in der Reihenfolge des Durchfahrens hinzufügen",
+                StationClicked = AddWaypoint,
+                WaypointsTransitionAllowed = true,
+                IsTerminating = true,
+            };
 
             this.AddSizeStateHandler();
         }
 
         public static TrainRouteForm EditPath(IInfo info, Train tra)
         {
-            var trf = new TrainRouteForm(info)
-            {
-                train = tra,
-                Path = tra.GetPath()
-            };
+            var trf = new TrainRouteForm(info, false, tra); // Disable waypoints, because we don't know old waypoints
             trf.Title = trf.Title.Replace("{train}", tra.TName);
 
-            trf.lineRenderer.FixedStatusString = "Durch Klick Stationen am Anfang/Ende des Laufwegs entfernen";
-            trf.lineRenderer.StationClicked += trf.ChangeRoute;
+            trf.Transition(trf.stateChangeRoute);
             trf.lineRenderer.SetHighlightedPath(trf.Path);
-            // Disable waypoints
-            trf.waypointsAllowed =
-                trf.waypointsCheckBox.Visible =
-                trf.waypointsDocuLink.Visible = false;
             return trf;
         }
 
         public static TrainRouteForm NewTrain(IInfo info)
         {
-            var trf = new TrainRouteForm(info)
+            var trf = new TrainRouteForm(info, info.Timetable.HasRouteCycles) // Set waypoints state according to timetable
             {
                 Title = "Fahrtstrecke für neuen Zug auswählen"
             };
 
-            trf.lineRenderer.FixedStatusString = "Startstation auswählen";
-            trf.lineRenderer.StationClicked += trf.SetRoute;
+            trf.Transition(trf.stateSetRoute);
 
             trf.closeButton.Text = "Weiter >>";
-            trf.closeButton.Enabled = false;
-            // Set waypoints state according to timetable
-            trf.waypointsAllowed =
-                trf.waypointsCheckBox.Visible =
-                trf.waypointsDocuLink.Visible = info.Timetable.HasRouteCycles;
-            // Waypoints can be edited only after creating initial route
-            trf.waypointsCheckBox.Enabled = false;
             return trf;
         }
 
-        private void ChangeRoute(object sender, EventArgs e)
+        #region State handlers
+        private void ChangeRoute(Station sta)
         {
-            var sta = (Station)sender;
-
-            if (Path == null || !Path.Any()) // We have no path. How did we come so far?
-            {
-                MessageBox.Show("Interner Fehler: Kein Pfad vorhanden, obwohl im Change-Route Modus. Dieses Fenster muss leider geschlossen werden!", "FPLedit", MessageBoxType.Error);
-                Close(DialogResult.Cancel);
+            if (PathSafeguard("Change-Route"))
                 return;
-            }
 
             if (!Path.Contains(sta)) // Not in path. Maybe add it.
             {
-                var pathfinder = new Pathfinder(info.Timetable);
-                if (waypointsAllowed && waypointsCheckBox.Checked.Value)
-                {
-                    if (!wayPoints.Contains(sta))
-                        wayPoints.Add(sta);
-                    Path = pathfinder.GetPath(Path.First(), Path.Last(), wayPoints.ToArray());
-                }
+                var pf = pathfinder.GetPath(sta, Path.First());
+                var pl = pathfinder.GetPath(Path.Last(), sta);
+
+                var intersectf = Path.Intersect(pf);
+                var intersectl = Path.Intersect(pl);
+
+                if (pf.Count > pl.Count && intersectl.Count() == 1)
+                    Path.AddRange(pl.Skip(1)); // Insert at start of path
+                else if (intersectf.Count() == 1)
+                    Path.InsertRange(0, pf.Take(pf.Count - 1)); // Insert at the end of path
                 else
-                {
-                    var pf = pathfinder.GetPath(sta, Path.First(), wayPoints.ToArray());
-                    var pl = pathfinder.GetPath(Path.Last(), sta);
+                    MessageBox.Show("Diese Station kann nicht zum Laufweg hinzugefügt werden, da dabei ein verzweigter Laufweg entstehen würde!", "FPLedit");
 
-                    var intersectf = Path.Intersect(pf);
-                    var intersectl = Path.Intersect(pl);
-
-                    if (pf.Count > pl.Count && intersectl.Count() == 1)
-                        Path.AddRange(pl.Skip(1)); // Insert at start of path
-                    else if (intersectf.Count() == 1)
-                        Path.InsertRange(0, pf.Take(pf.Count - 1)); // Insert at the end of path
-                    else
-                        MessageBox.Show("Diese Station kann nicht zum Laufweg hinzugefügt werden, da dabei ein verzweigter Laufweg entstehen würde!", "FPLedit");
-                }
             }
             // From here on, we search for reasons, NOT removing the station.
             else if (sta != Path.Last() && sta != Path.First()) // Currently already in path, but not at both ends
@@ -136,59 +150,64 @@ namespace FPLedit.Editor.Network
             lineRenderer.SetHighlightedPath(Path);
         }
 
-        private void SetRoute(object sender, EventArgs e)
+        private void AddWaypoint(Station sta)
         {
-            if (staStart == null)
+            if (PathSafeguard("Add-Waypoint"))
+                return;
+
+            if (!wayPoints.Contains(sta))
+                wayPoints.Add(sta);
+            Path = pathfinder.GetPath(Path.First(), Path.Last(), wayPoints.ToArray());
+        }
+
+        private void SetRoute(Station sta)
+        {
+            if (!Path.Any()) // There is no current path, so we set the first station
             {
-                staStart = (Station)sender;
-                lineRenderer.SetHighlightedPath(new[] { staStart });
+                Path = new[] { sta }.ToList();
                 lineRenderer.FixedStatusString = "Zielstation auswählen";
             }
-            else if (staEnd == null)
+            else if (Path.Count() == 1) // We already have the first station set, so set the end of path
             {
-                staEnd = (Station)sender;
-
-                if (staEnd == staStart)
+                if (sta == Path.First())
                 {
                     MessageBox.Show("Züge mit gleicher Start- und Zielstation sind nicht möglich!");
-                    staEnd = null;
                     return;
                 }
 
-                var pathfinder = new Pathfinder(info.Timetable);
-                Path = pathfinder.GetPath(staStart, staEnd);
-                lineRenderer.SetHighlightedPath(Path);
-                closeButton.Enabled = true;
+                Path = pathfinder.GetPath(Path.First(), sta);
 
-                lineRenderer.FixedStatusString = "";
-
-                // Wechseln in den Änderungsmodus
-                lineRenderer.StationClicked -= SetRoute;
-                lineRenderer.StationClicked += ChangeRoute;
-                // Waypoints can be edited only after creating initial route, and only if available
-                waypointsCheckBox.Enabled = waypointsAllowed;
+                Transition(stateChangeRoute);
             }
         }
 
-        //TODO: Removed because of bugs. I think this is not urgent, as this feature is probably not used by anyone. Also re-enable in XAML.
-        //private void ResetRoute(object sender, EventArgs e)
-        //{
-        //    staStart = null;
-        //    staEnd = null;
+        private void InitSetRoute() // Initialization for state "SetRoute"
+        {
+            Path = new List<Station>();
+            wayPoints.Clear();
+        }
 
-        //    Path = new List<Station>();
+        private bool PathSafeguard(string mode) // Helper, used in states
+        {
+            if (Path == null || !Path.Any()) // We have no path. How did we come so far?
+            {
+                MessageBox.Show($"Interner Fehler: Kein Pfad vorhanden, obwohl im {mode} Modus. Dieses Fenster muss leider geschlossen werden!", "FPLedit", MessageBoxType.Error);
+                Close(DialogResult.Cancel);
+            }
+            return Path == null || !Path.Any();
+        }
+        #endregion
 
-        //    lineRenderer.ClearHighlightedPath();
-        //    closeButton.Enabled = false;
-        //    lineRenderer.FixedStatusString = "Startstation auswählen";
+        private void ResetRoute(object sender, EventArgs e)
+            => Transition(stateSetRoute); // Just switch back to state SetRoute
 
-        //    if (train != null)
-        //    {
-        //        // Wechsel in den Neu Setzen-Modus
-        //        lineRenderer.StationClicked -= ChangeRoute;
-        //        lineRenderer.StationClicked += SetRoute;
-        //    }
-        //}
+        private void WaypointsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (globalWaypointsAllowed && waypointsCheckBox.Checked.Value)
+                Transition(stateAddWaypoints);
+            else
+                Transition(stateChangeRoute);
+        }
 
         private void CloseButton_Click(object sender, EventArgs e)
         {
@@ -203,7 +222,7 @@ namespace FPLedit.Editor.Network
             {
                 var ardps = train.GetArrDeps();
 
-                foreach (var ardp in ardps) // Alle alten Stationen entfernen
+                foreach (var ardp in ardps) // Remove all old stations
                     train.RemoveArrDep(ardp.Key);
 
                 train.AddAllArrDeps(Path);
@@ -228,5 +247,34 @@ namespace FPLedit.Editor.Network
                 lineRenderer.DispatchKeystroke(e);
             base.OnKeyDown(e);
         }
+
+        #region State machine
+        private RouteEditState currentState;
+
+        private void Transition(RouteEditState targetState)
+        {
+            lineRenderer.FixedStatusString = targetState.InitialStatusString;
+            closeButton.Enabled = targetState.IsTerminating;
+            waypointsCheckBox.Enabled = targetState.WaypointsTransitionAllowed && globalWaypointsAllowed;
+            targetState.StateInitialize?.Invoke();
+            currentState = targetState;
+        }
+
+        private void HandleStationClick(object sender, EventArgs e)
+        {
+            if (currentState == null || currentState.StationClicked == null)
+                return;
+            currentState.StationClicked((Station)sender);
+        }
+
+        private class RouteEditState
+        {
+            public string InitialStatusString;
+            public Action<Station> StationClicked;
+            public bool WaypointsTransitionAllowed;
+            public bool IsTerminating;
+            public Action StateInitialize;
+        }
+        #endregion
     }
 }
