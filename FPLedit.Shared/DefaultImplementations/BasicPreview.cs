70a99c8ff9408b68b16d50959af8ae1e1579a163
Manuel Huber <ich@manuelhu.de>
2020-02-17 19:22:16 +0100

    Execute all BasicPreviews asynchronously

--- FPLedit.Shared/DefaultImplementations/BasicPreview.cs
+++ FPLedit.Shared/DefaultImplementations/BasicPreview.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using Eto.Forms;
 using FPLedit.Shared.Helpers;
 
 namespace FPLedit.Shared.DefaultImplementations
@@ -7,25 +9,31 @@ namespace FPLedit.Shared.DefaultImplementations
     {
         private readonly string templateType;
         private readonly IExport export;
-        
+
         public string DisplayName { get; }
-        
+
         public BasicPreview(string templateType, string display, IExport export)
         {
             this.templateType = templateType;
             this.export = export;
             DisplayName = display;
         }
-        
+
         public void Show(IPluginInterface pluginInterface)
         {
             string path = pluginInterface.GetTemp(templateType + ".html");
 
-            bool tryoutConsole = pluginInterface.Settings.Get<bool>(templateType + ".console");
-            bool success = export.SafeExport(pluginInterface.Timetable, path, pluginInterface, tryoutConsole ? new [] { BasicTemplateExport.FLAG_TYROUT_CONSOLE } : Array.Empty<string>());
+            var tryoutConsole = pluginInterface.Settings.Get<bool>(templateType + ".console");
 
-            if (success)
-                OpenHelper.Open(path);
+            var clone = pluginInterface.Timetable.Clone();
+
+            var tsk = export.GetAsyncSafeExport(clone, path, pluginInterface, tryoutConsole ? new[] {BasicTemplateExport.FLAG_TYROUT_CONSOLE} : Array.Empty<string>());
+            tsk.ContinueWith((t, o) =>
+            {
+                if (t.Result)
+                    Application.Instance.Invoke(() => OpenHelper.Open(path));
+            }, null, TaskScheduler.Default);
+            tsk.Start();
         }
     }
 }
--- FPLedit.Shared/Ext/IExport.cs
+++ FPLedit.Shared/Ext/IExport.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace FPLedit.Shared
 {
+    /// <summary>
+    /// Basic interface to provide exporter capabalities.
+    /// </summary>
+    /// <remarks>See <see cref="FPLedit.Shared.DefaultImplementations.BasicTemplateExport"/> for a default implementation.</remarks>
     public interface IExport
     {
+        /// <summary>
+        /// Invokes the exporter.
+        /// </summary>
+        /// <param name="tt">A readonly copy of the current timetable.</param>
+        /// <param name="stream"></param>
+        /// <param name="pluginInterface">A reduced PluginInterface that provides limited core features from FPledit.</param>
+        /// <param name="flags">Exporter flags.</param>
+        /// <returns></returns>
+        /// <remarks>This method must be thread-safe and MUST NOT call into UI directly, as it might be called on a non-UI thread.</remarks>
         bool Export(Timetable tt, Stream stream, IReducedPluginInterface pluginInterface, string[] flags = null);
 
         string Filter { get; }
@@ -29,5 +43,8 @@ namespace FPLedit.Shared
                 return false;
             }
         }
+
+        public static Task<bool> GetAsyncSafeExport(this IExport exp, Timetable tt, string filename, IReducedPluginInterface pluginInterface, string[] flags = null) 
+            => new Task<bool>(() => exp.SafeExport(tt, filename, pluginInterface, flags));
     }
 }
