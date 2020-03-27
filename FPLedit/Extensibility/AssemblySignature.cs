31e3a4ae9dd4141c6066b6caf481d158b18cc20c
Manuel Huber <ich@manuelhu.de>
2020-03-27 23:26:06 +0100

    Extensions signing: Switch to compile-time binary-array generation

--- FPLedit/Extensibility/AssemblySignature.cs
+++ FPLedit/Extensibility/AssemblySignature.cs
@@ -1,27 +1,29 @@
-﻿using FPLedit.Shared.UI;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace FPLedit.Extensibility
 {
     internal sealed partial class AssemblySignatureVerifier
     {
+        // hashAlgorithm, pubkeyExponent, pubkeyModulus, generatedSignatures are defined in compiler generated file!
+        
         private readonly RSAParameters pubkey;
 
         public AssemblySignatureVerifier()
         {
-            var x = new XmlSerializer(typeof(RSAParameters));
-            using (var reader = new XmlTextReader(publicKey, XmlNodeType.Document, null) { DtdProcessing = DtdProcessing.Prohibit, XmlResolver = null })
-                pubkey = (RSAParameters) x.Deserialize(reader);
+            pubkey = new RSAParameters()
+            {
+                Modulus = pubKeyModulus,
+                Exponent = pubKeyExponent,
+            };
         }
 
         internal SecurityContext Validate(string fn)
         {
+            if (fn == null)
+                return SecurityContext.ThirdParty;
+            
             var basename = Path.GetFileName(fn);
             var signature = generatedSignatures.FirstOrDefault(s => s.FileName == basename);
 
@@ -30,15 +32,15 @@ namespace FPLedit.Extensibility
 
             var bytes = File.ReadAllBytes(fn);
 
-            using (var sha512 = SHA256.Create())
+            using (var hasher = HashAlgorithm.Create(hashAlgorithm))
             using (var rsa = new RSACryptoServiceProvider())
             {
-                var hash = sha512.ComputeHash(bytes);
+                var hash = hasher.ComputeHash(bytes);
 
                 rsa.ImportParameters(pubkey);
 
                 var sigDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
-                sigDeformatter.SetHashAlgorithm("SHA256");
+                sigDeformatter.SetHashAlgorithm(hashAlgorithm);
                 return sigDeformatter.VerifySignature(hash, signature.Signature) ? SecurityContext.Official : SecurityContext.ThirdParty;
             }
         }
@@ -48,12 +50,10 @@ namespace FPLedit.Extensibility
             public readonly string FileName;
             public readonly byte[] Signature;
 
-            public AssemblySignature(string fn, string hexSignature)
+            public AssemblySignature(string fn, byte[] signature)
             {
                 FileName = fn;
-                Signature = new byte[hexSignature.Length / 2];
-                for (var i = 0; i < hexSignature.Length; i += 2)
-                    Signature[i / 2] = Convert.ToByte(hexSignature.Substring(i, 2), 16);
+                Signature = signature;
             }
         }
     }
