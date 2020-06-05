FPLedit/FPLedit

commit 216f8ecabd64ff6bb42ee333d09a254f799ddb81

Remove direct entity clone

FPLedit.Shared/Station.cs
@@ -164,5 +164,14 @@ namespace FPLedit.Shared
             Routes = list.ToArray();
             return true;
         }
+
+        /// <summary>
+        /// Create a copy of this Station. This is not a deep copy, as it preserves the parent timetable.
+        /// </summary>
+        public Station Copy()
+        {
+            var xml = XMLEntity.XClone();
+            return new Station(xml, _parent);
+        }
     }
 }

FPLedit/Editor/EditStationForm.xeto.cs
@@ -89,7 +89,7 @@ namespace FPLedit.Editor
             existingStation = false;
             Station = new Station(tt);
 
-            trackStation = Station.Clone<Station>();
+            trackStation = Station.Copy();
             stationRenderer.Station = trackStation;
             stationRenderer.Route = route;
 
@@ -111,7 +111,7 @@ namespace FPLedit.Editor
 
             existingStation = true;
 
-            trackStation = Station.Clone<Station>();
+            trackStation = Station.Copy();
             stationRenderer.Station = trackStation;
             stationRenderer.Route = route;
 

FPLedit.Shared/ArrDep.cs
@@ -156,5 +156,15 @@ namespace FPLedit.Shared
                 ShuntMoves.Add(newShunt);
             }
         }
+
+        /// <summary>
+        /// Create a copy of this ArrDep. This is not a deep copy, as it preserves the parent timetable.
+        /// </summary>
+        public ArrDep Copy()
+        {
+            var copy = new ArrDep(_parent);
+            copy.ApplyCopy(this);
+            return copy;
+        }
     }
 }

FPLedit.Shared/Entity.cs
@@ -60,12 +60,6 @@ namespace FPLedit.Shared
         public void RemoveAttribute(string key)
             => XMLEntity.RemoveAttribute(key);
 
-        /// <summary>
-        /// Create a deep clone of the current entity object.
-        /// </summary>
-        /// <typeparam name="T">Type of the current entity, needs to be specified.</typeparam>
-        public T Clone<T>() where T : Entity => (T)this.DeepClone();
-
         protected void SetNotEmptyTimeAttribute(string key, TimeEntry time)
         {
             var t = time.ToShortTimeString();

FPLedit.Shared/Helpers/StationMoveHelper.cs
@@ -113,7 +113,7 @@ namespace FPLedit.Shared.Helpers
 
                 // This train runs over this station on this route, so we need to update it.
                 var arrDep = tra.GetArrDep(sta);
-                ardeps[tra] = arrDep.Clone<ArrDep>();
+                ardeps[tra] = arrDep.Copy();
                 tra.RemoveArrDep(sta);
             }
 

FPLedit.Shared/Timetable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Force.DeepCloner;
 
 namespace FPLedit.Shared
 {
@@ -636,7 +637,7 @@ namespace FPLedit.Shared
         #endregion
         
         /// <inheritdoc />
-        public Timetable Clone() => Clone<Timetable>();
+        public Timetable Clone() => this.DeepClone();
 
         [DebuggerStepThrough]
         public override string ToString()

FPLedit.Shared/XMLEntity.cs
@@ -81,6 +81,20 @@ namespace FPLedit.Shared
                 return false;
             return Children.All(c => c.XDiff(other.Children[Children.IndexOf(c)]));
         }
+
+        /// <summary>
+        /// Create a copy of this XML entities' XML tree.
+        /// </summary>
+        public XMLEntity XClone()
+        {
+            var clone = new XMLEntity(XName);
+            foreach (var attr in Attributes)
+                clone.SetAttribute(attr.Key, attr.Value);
+            foreach (var child in Children)
+                clone.Children.Add(child.XClone());
+
+            return clone;
+        }
     }
 
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]

FPLedit/Editor/TimetableEditor/ShuntForm.xeto.cs
@@ -50,7 +50,7 @@ namespace FPLedit.Editor.TimetableEditor
 
             this.AddSizeStateHandler();
 
-            shuntBackup = arrDep.ShuntMoves.Select(s => s.Clone<ShuntMove>()).ToList();
+            shuntBackup = arrDep.Copy().ShuntMoves.ToList();
 
             RefreshList();
         }

commit ae2cdc01d1fe33431d7332b2081d65c7454b7ec2

Fix station track editing (regression from 216f8eca)
* next and previous station names show up correctly
* Use output properties and don't use deep copied station any more.
* => This resolves dissociating from the parent timetable
* API Change: Station.Copy moved to Track.Copy;
  Stations can't be copied in a safe way (without dissociating
  from some parent properties lie Timetable.Stations or their route) anyways
* API Addition: New standalone RVCs

FPLedit.Shared/RouteValueCollection.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Force.DeepCloner;
 using FPLedit.Shared.Helpers;
 
 namespace FPLedit.Shared
 {
     /// <summary>
     /// A RouteValueCollection (RVC) allows to define properties which allow for different values for each individual
-    /// route of a Station.
+    /// route of a Station. This RVC is associated with a XML attribute.
     /// </summary>
+    /// <remarks>Reference integrity is not guaranteed.</remarks>
     /// <typeparam name="T">Any type that is transformable to a string with reasonable effort.</typeparam>
     [Templating.TemplateSafe]
-    public class RouteValueCollection<T>
+    public class RouteValueCollection<T> : IRouteValueCollection<T>
     {
         private static readonly EscapeSplitHelper escape = new EscapeSplitHelper(';');
         
@@ -23,6 +26,26 @@ namespace FPLedit.Shared
         private readonly Func<T, string> convFrom;
         private readonly bool optional;
         private readonly T convDefault;
+        
+        /// <inheritdoc />
+        public IReadOnlyDictionary<int, T> Values => new ReadOnlyDictionary<int, T>(values);
+
+        /// <summary>
+        /// Creates a standalone copy of this RVC.
+        /// </summary>
+        public IRouteValueCollection<T> ToStandalone() 
+            => new StandaloneRouteValueCollection<T>(values.DeepClone(), convDefault);
+
+        /// <summary>
+        /// Copies all values from a (standalone) RVC back into this instance. This clears all values of this instance before.
+        /// </summary>
+        public void FromStandalone(IRouteValueCollection<T> standalone)
+        {
+            values.Clear();
+            foreach (var val in standalone.Values)
+                values[val.Key] = val.Value;
+            Write();
+        }
 
         /// <summary>
         /// Creates a new RVC.
@@ -62,9 +85,7 @@ namespace FPLedit.Shared
         {
         }
 
-        /// <summary>
-        /// Return the value - or the default value, if no value has been set - corresponding to the given route.
-        /// </summary>
+        /// <inheritdoc />
         public T GetValue(int route)
         {
             if (values.TryGetValue(route, out T val))
@@ -72,9 +93,7 @@ namespace FPLedit.Shared
             return convDefault;
         }
 
-        /// <summary>
-        /// Set the value corresponding to the given route.
-        /// </summary>
+        /// <inheritdoc />
         public void SetValue(int route, T val)
         {
             values[route] = val;
@@ -103,7 +122,7 @@ namespace FPLedit.Shared
         /// </summary>
         private void ParseLinear()
         {
-            var toParse = entity.GetAttribute<string>(attr, null);
+            var toParse = entity.GetAttribute<string>(attr);
             if (optional && toParse == null)
                 return;
             
@@ -122,7 +141,7 @@ namespace FPLedit.Shared
             if (optional && !values.Any())
                 return;
 
-            var text = "";
+            string text;
             var t = forceType ?? tt.Type;
             if (t == TimetableType.Linear)
                 text = convFrom(GetValue(Timetable.LINEAR_ROUTE_ID));
@@ -134,14 +153,10 @@ namespace FPLedit.Shared
             entity.SetAttribute(attr, text);
         }
 
-        /// <summary>
-        /// Returns whether this RVC contains the given value at any route.
-        /// </summary>
+        /// <inheritdoc />
         public bool ContainsValue(T value) => values.ContainsValue(value);
 
-        /// <summary>
-        /// Replace all opccurences of <paramref name="oldVal"/> with <paramref name="newVal" /> on all routes. 
-        /// </summary>
+        /// <inheritdoc />
         public void ReplaceAllValues(T oldVal, T newVal)
         {
             for (int i = 0; i < values.Count; i++)
@@ -151,6 +166,84 @@ namespace FPLedit.Shared
                     SetValue(kvp.Key, newVal);
             }
         }
+    }
+
+    /// <inheritdoc />
+    public sealed class StandaloneRouteValueCollection<T> : IRouteValueCollection<T>
+    {
+        private readonly Dictionary<int, T> values;
+        private readonly T defaultValue;
+
+        internal StandaloneRouteValueCollection(Dictionary<int, T> values, T defaultValue)
+        {
+            this.values = values;
+            this.defaultValue = defaultValue;
+        }
+        
+        /// <inheritdoc />
+        public IReadOnlyDictionary<int, T> Values => new ReadOnlyDictionary<int, T>(values);
+
+        /// <inheritdoc />
+        public T GetValue(int route)
+        {
+            if (values.TryGetValue(route, out T val))
+                return val;
+            return defaultValue;
+        }
+
+        /// <inheritdoc />
+        public void SetValue(int route, T val)
+        {
+            values[route] = val;
+        }
+        
+        /// <inheritdoc />
+        public bool ContainsValue(T value) => values.ContainsValue(value);
+
+        /// <inheritdoc />
+        public void ReplaceAllValues(T oldVal, T newVal)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                var kvp = values.ElementAt(i);
+                if (kvp.Value.Equals(oldVal))
+                    SetValue(kvp.Key, newVal);
+            }
+        }
+    }
+
+    /// <summary>
+    /// A RouteValueCollection (RVC) allows to define properties which allow for different values for each individual
+    /// route of a Station.
+    /// </summary>
+    /// <remarks>Reference integrity is not guaranteed.</remarks>
+    /// <typeparam name="T">Any type that is transformable to a string with reasonable effort.</typeparam>
+    public interface IRouteValueCollection<T>
+    {
+        /// <summary>
+        /// Returns all current values of this RouteValueCollection.
+        /// </summary>
+        IReadOnlyDictionary<int, T> Values { get; }
+        
+        /// <summary>
+        /// Return the value - or the default value, if no value has been set - corresponding to the given route.
+        /// </summary>
+        T GetValue(int route);
+
+        /// <summary>
+        /// Set the value corresponding to the given route.
+        /// </summary>
+        void SetValue(int route, T val);
+        
+        /// <summary>
+        /// Returns whether this RVC contains the given value at any route.
+        /// </summary>
+        bool ContainsValue(T value);
+
+        /// <summary>
+        /// Replace all opccurences of <paramref name="oldVal"/> with <paramref name="newVal" /> on all routes. 
+        /// </summary>
+        void ReplaceAllValues(T oldVal, T newVal);
     }
 }
 

FPLedit.Shared/Station.cs
@@ -164,14 +164,5 @@ namespace FPLedit.Shared
             Routes = list.ToArray();
             return true;
         }
-
-        /// <summary>
-        /// Create a copy of this Station. This is not a deep copy, as it preserves the parent timetable.
-        /// </summary>
-        public Station Copy()
-        {
-            var xml = XMLEntity.XClone();
-            return new Station(xml, _parent);
-        }
     }
 }

FPLedit.Shared/Track.cs
@@ -22,5 +22,14 @@
             get => GetAttribute<string>("name");
             set => SetAttribute("name", value);
         }
+        
+        /// <summary>
+        /// Create a copy of this Track. This is not a deep copy, as it copies only the xml tree below, and it preserves the parent timetable.
+        /// </summary>
+        public Track Copy()
+        {
+            var xml = XMLEntity.XClone();
+            return new Track(xml, _parent);
+        }
     }
 }

FPLedit/Editor/EditStationForm.xeto.cs
@@ -26,7 +26,6 @@ namespace FPLedit.Editor
         public float Position { get; private set; }
 
         private readonly bool existingStation;
-        private readonly Station trackStation;
 
         private int stationRendererHeight, stationRendererWidth;
 
@@ -89,9 +88,7 @@ namespace FPLedit.Editor
             existingStation = false;
             Station = new Station(tt);
 
-            trackStation = Station.Copy();
-            stationRenderer.Station = trackStation;
-            stationRenderer.Route = route;
+            stationRenderer.InitializeWithStation(route, Station);
 
             if (tt.Version.Compare(TimetableVersion.JTG3_1) < 0)
                 stationRenderer.Visible = false;
@@ -111,9 +108,7 @@ namespace FPLedit.Editor
 
             existingStation = true;
 
-            trackStation = Station.Copy();
-            stationRenderer.Station = trackStation;
-            stationRenderer.Route = route;
+            stationRenderer.InitializeWithStation(route, Station);
 
             if (station._parent.Version.Compare(TimetableVersion.JTG3_1) < 0)
                 stationRenderer.Visible = false;
@@ -167,10 +162,10 @@ namespace FPLedit.Editor
             }
             
             // Update track data.
-            Station.DefaultTrackLeft.SetValue(route, trackStation.DefaultTrackLeft.GetValue(route));
-            Station.DefaultTrackRight.SetValue(route, trackStation.DefaultTrackRight.GetValue(route));
+            Station.DefaultTrackLeft.FromStandalone(stationRenderer.DefaultTrackLeft);
+            Station.DefaultTrackRight.FromStandalone(stationRenderer.DefaultTrackRight);
             Station.Tracks.Clear();
-            foreach (var track in trackStation.Tracks)
+            foreach (var track in stationRenderer.Tracks)
                 Station.Tracks.Add(track);
             foreach (var rename in stationRenderer.TrackRenames)
                 Station._parent._InternalRenameAllTrainTracksAtStation(Station, rename.Key, rename.Value);

FPLedit/Editor/Rendering/StationRenderer.cs
@@ -4,6 +4,7 @@ using FPLedit.Shared;
 using FPLedit.Shared.Rendering;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -21,37 +22,44 @@ namespace FPLedit.Editor.Rendering
 
         private RenderBtn<Track> editingButton;
 
-        private Station _station;
-        public Station Station
-        {
-            get => _station;
-            set
-            {
-                _station = value;
-                this.Invalidate();
-            }
-        }
+        // Injected by InitializeWithStation.
+        private int routeIndex;
+        private Station station;
+        private Timetable tt;
 
-        private int _route;
-        public int Route
-        {
-            get => _route;
-            set
-            {
-                _route = value;
-                this.Invalidate();
-            }
-        }
+        #region Result properties
+        
+        public IRouteValueCollection<string> DefaultTrackLeft { get; private set; }
+
+        public IRouteValueCollection<string> DefaultTrackRight { get; private set; }
+
+        public ObservableCollection<Track> Tracks { get; private set; }
 
         public Dictionary<string, string> TrackRenames { get; } = new Dictionary<string, string>();
         
-        private List<string> trackRemoves = new List<string>();
-        public IList<string> TrackRemoves => trackRemoves.AsReadOnly();
+        private readonly List<string> trackRemoves = new List<string>();
+        public IEnumerable<string> TrackRemoves => trackRemoves.AsReadOnly();
+        
+        #endregion
 
         public StationRenderer()
         {
             textColor = SystemColors.ControlText;
             bgColor = SystemColors.ControlBackground;
+        }
+        
+        public void InitializeWithStation(int route, Station value)
+        {
+            routeIndex = route;
+            station = value;
+
+            tt = value._parent;
+
+            DefaultTrackLeft = value.DefaultTrackLeft.ToStandalone();
+            DefaultTrackRight = value.DefaultTrackLeft.ToStandalone();
+            Tracks = new ObservableCollection<Track>(value.Tracks.Select(t => t.Copy()));
+
+            Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -63,13 +71,13 @@ namespace FPLedit.Editor.Rendering
             int midx = Width / 2;
             
             // Richtungsangaben ermitteln
-            var route = _station._parent.GetRoute(_route).Stations;
-            var staIdx = route.IndexOf(_station);
+            var route = station._parent.GetRoute(routeIndex).Stations;
+            var staIdx = route.IndexOf(station);
             var prev = route.ElementAtOrDefault(staIdx - 1);
             var next = route.ElementAtOrDefault(staIdx + 1);
 
-            bool disableRight = _station._parent.Type == TimetableType.Network && next == null;
-            bool disableLeft = _station._parent.Type == TimetableType.Network && prev == null;
+            bool disableRight = tt.Type == TimetableType.Network && next == null; //TODO: Why only disable on network?
+            bool disableLeft = tt.Type == TimetableType.Network && prev == null;
 
             // Richtungsangaben zeichnen
             if (prev != null)
@@ -80,17 +88,17 @@ namespace FPLedit.Editor.Rendering
                 e.Graphics.DrawText(font, textColor, Width - 5 - nextSize.Width, 5, "nach " + next.SName);
             }
 
-            var leftdefaultTrack = _station.Tracks.IndexOf(_station.Tracks.FirstOrDefault(t => t.Name == _station.DefaultTrackLeft.GetValue(_route)));
-            var rightdefaultTrack = _station.Tracks.IndexOf(_station.Tracks.FirstOrDefault(t => t.Name == _station.DefaultTrackRight.GetValue(_route)));
+            var leftdefaultTrack = Tracks.IndexOf(Tracks.FirstOrDefault(t => t.Name == DefaultTrackLeft.GetValue(routeIndex)));
+            var rightdefaultTrack = Tracks.IndexOf(Tracks.FirstOrDefault(t => t.Name == DefaultTrackRight.GetValue(routeIndex)));
 
             // Netzwerk: Falls noch keine Angabe: Standardgleise setzen
-            if (_station._parent.Type == TimetableType.Network && _station.Tracks.Any())
+            if (tt.Type == TimetableType.Network && Tracks.Any())
             {
                 if (disableLeft)
                     leftdefaultTrack = 0;
                 else if (leftdefaultTrack == -1)
                 {
-                    MoveDefaultTrack(_station.DefaultTrackLeft, _station.Tracks.First(), 0);
+                    MoveDefaultTrack(DefaultTrackLeft, Tracks.First(), 0);
                     leftdefaultTrack = 0;
                 }
 
@@ -98,7 +106,7 @@ namespace FPLedit.Editor.Rendering
                     rightdefaultTrack = 0;
                 else if (rightdefaultTrack == -1)
                 {
-                    MoveDefaultTrack(_station.DefaultTrackRight, _station.Tracks.First(), 0);
+                    MoveDefaultTrack(DefaultTrackRight, Tracks.First(), 0);
                     rightdefaultTrack = 0;
                 }
             }
@@ -106,9 +114,9 @@ namespace FPLedit.Editor.Rendering
             int y = 30;
             int maxIndent = 0;
 
-            foreach (var track in _station.Tracks)
+            foreach (var track in Tracks)
             {
-                var trackIndex = _station.Tracks.IndexOf(track);
+                var trackIndex = Tracks.IndexOf(track);
 
                 // Einrückung des Gleisvorfeldes berehcnen
                 var leftIndent = Math.Abs(leftdefaultTrack - trackIndex) * INDENT + 60;
@@ -142,9 +150,9 @@ namespace FPLedit.Editor.Rendering
                 buttons.Add(nameBtn);
 
                 // Netzwerk: Standardgleise anderer Routen gestrichelt zeichnen.
-                if (_station.DefaultTrackLeft.ContainsValue(track.Name) && leftdefaultTrack != trackIndex)
+                if (DefaultTrackLeft.ContainsValue(track.Name) && leftdefaultTrack != trackIndex)
                     e.Graphics.DrawLine(dashedPen, 0, y, leftIndent, y);
-                if (_station.DefaultTrackRight.ContainsValue(track.Name) && rightdefaultTrack != trackIndex)
+                if (DefaultTrackRight.ContainsValue(track.Name) && rightdefaultTrack != trackIndex)
                     e.Graphics.DrawLine(dashedPen, rightIndent, y, Width, y);
 
                 // Aktions-Buttons hinzufügen
@@ -162,17 +170,17 @@ namespace FPLedit.Editor.Rendering
                 if (trackIndex == leftdefaultTrack && !disableLeft)
                 {
                     var leftUpBtn = GetButton("▲", track, 10, y);
-                    leftUpBtn.Click += (s, x) => MoveDefaultTrack(_station.DefaultTrackLeft, ((RenderBtn<Track>)s).Tag, -1);
+                    leftUpBtn.Click += (s, x) => MoveDefaultTrack(DefaultTrackLeft, ((RenderBtn<Track>)s).Tag, -1);
                     var leftDownBtn = GetButton("▼", track, 30, y);
-                    leftDownBtn.Click += (s, x) => MoveDefaultTrack(_station.DefaultTrackLeft, ((RenderBtn<Track>)s).Tag, 1);
+                    leftDownBtn.Click += (s, x) => MoveDefaultTrack(DefaultTrackLeft, ((RenderBtn<Track>)s).Tag, 1);
                 }
 
                 if (trackIndex == rightdefaultTrack && !disableRight)
                 {
                     var rightUpButton = GetButton("▲", track, Width - 46, y);
-                    rightUpButton.Click += (s, x) => MoveDefaultTrack(_station.DefaultTrackRight, ((RenderBtn<Track>)s).Tag, -1);
+                    rightUpButton.Click += (s, x) => MoveDefaultTrack(DefaultTrackRight, ((RenderBtn<Track>)s).Tag, -1);
                     var rightDownBtn = GetButton("▼", track, Width - 26, y);
-                    rightDownBtn.Click += (s, x) => MoveDefaultTrack(_station.DefaultTrackRight, ((RenderBtn<Track>)s).Tag, 1);
+                    rightDownBtn.Click += (s, x) => MoveDefaultTrack(DefaultTrackRight, ((RenderBtn<Track>)s).Tag, 1);
                 }
 
                 y += LINE_HEIGHT;
@@ -184,7 +192,7 @@ namespace FPLedit.Editor.Rendering
             buttons.Add(addBtn);
             addBtn.Click += AddBtn_Click;
 
-            var newHeight = (_station.Tracks.Count) * LINE_HEIGHT + 50;
+            var newHeight = (Tracks.Count) * LINE_HEIGHT + 50;
             if (newHeight > Height)
                 this.Height = newHeight;
 
@@ -197,13 +205,13 @@ namespace FPLedit.Editor.Rendering
             base.OnPaint(e);
         }
 
-        private void MoveDefaultTrack(RouteValueCollection<string> property, Track current, int offset)
+        private void MoveDefaultTrack(IRouteValueCollection<string> property, Track current, int offset)
         {
-            var idx = _station.Tracks.IndexOf(current) + offset;
-            if (idx < 0 || idx > _station.Tracks.Count - 1)
+            var idx = Tracks.IndexOf(current) + offset;
+            if (idx < 0 || idx > Tracks.Count - 1)
                 return;
-            var next = _station.Tracks[idx];
-            property.SetValue(_route, next.Name);
+            var next = Tracks[idx];
+            property.SetValue(routeIndex, next.Name);
             Invalidate();
         }
 
@@ -211,27 +219,27 @@ namespace FPLedit.Editor.Rendering
         {
             var regex = new Regex(@"^Gleis (\d+)$", RegexOptions.Compiled);
             var maxTrack = 0;
-            var matchedTracks = _station.Tracks.Select(t => regex.Match(t.Name)).Where(m => m.Success);
+            var matchedTracks = Tracks.Select(t => regex.Match(t.Name)).Where(m => m.Success).ToArray();
             if (matchedTracks.Any())
                 maxTrack = matchedTracks.Select(m => int.Parse(m.Groups[1].Value)).Max();
-            var track = new Track(_station._parent)
+            var track = new Track(tt)
             {
                 Name = "Gleis " + (maxTrack + 1)
             };
 
-            if (!_station.Tracks.Any())
+            if (!Tracks.Any())
             {
-                _station.DefaultTrackLeft.SetValue(_route, track.Name);
-                _station.DefaultTrackRight.SetValue(_route, track.Name);
+                DefaultTrackLeft.SetValue(routeIndex, track.Name);
+                DefaultTrackRight.SetValue(routeIndex, track.Name);
             }
-            _station.Tracks.Add(track);
+            Tracks.Add(track);
 
             Invalidate();
         }
 
         private void NameBtn_Click(object sender, EventArgs e)
         {
-            editingButton = (RenderBtn<Track>)sender;
+            editingButton = (RenderBtn<Track>) sender;
             var oldName = editingButton.Tag.Name;
             var newName = Shared.UI.InputBox.Query(ParentWindow, "Gleisnamen bearbeiten", oldName);
             if (newName != null && newName != oldName)
@@ -240,7 +248,7 @@ namespace FPLedit.Editor.Rendering
 
         private void CommitNameEdit(string oldName, string newName)
         {
-            var duplicate = _station.Tracks.Any(t => t != editingButton.Tag && t.Name == newName);
+            var duplicate = Tracks.Any(t => t != editingButton.Tag && t.Name == newName);
             if (duplicate)
             {
                 MessageBox.Show($"Ein Gleis mit der Bezeichnung {newName} ist bereits vorhanden. Bitte wählen Sie einen anderen Namen!", MessageBoxType.Error);
@@ -248,8 +256,8 @@ namespace FPLedit.Editor.Rendering
             }
 
             // Streckengleise umbenennen
-            _station.DefaultTrackLeft.ReplaceAllValues(editingButton.Tag.Name, newName);
-            _station.DefaultTrackRight.ReplaceAllValues(editingButton.Tag.Name, newName);
+            DefaultTrackLeft.ReplaceAllValues(editingButton.Tag.Name, newName);
+            DefaultTrackRight.ReplaceAllValues(editingButton.Tag.Name, newName);
 
             // Ankunfts- und Abfahrtsgleise zum umbenennen stagen
             if (TrackRenames.ContainsKey(newName))
@@ -271,31 +279,31 @@ namespace FPLedit.Editor.Rendering
         private void DownBtn_Click(object sender, EventArgs e)
         {
             var btn = (RenderBtn<Track>)sender;
-            var idx = _station.Tracks.IndexOf(btn.Tag);
-            if (idx == _station.Tracks.Count - 1)
+            var idx = Tracks.IndexOf(btn.Tag);
+            if (idx == Tracks.Count - 1)
                 return;
-            _station.Tracks.Move(idx, idx + 1);
+            Tracks.Move(idx, idx + 1);
             Invalidate();
         }
 
         private void UpBtn_Click(object sender, EventArgs e)
         {
             var btn = (RenderBtn<Track>)sender;
-            var idx = _station.Tracks.IndexOf(btn.Tag);
+            var idx = Tracks.IndexOf(btn.Tag);
             if (idx == 0)
                 return;
-            _station.Tracks.Move(idx, idx - 1);
+            Tracks.Move(idx, idx - 1);
             Invalidate();
         }
 
         private void DeleteBtn_Click(object sender, EventArgs e)
         {
             var btn = (RenderBtn<Track>)sender;
-            _station.Tracks.Remove(btn.Tag);
+            Tracks.Remove(btn.Tag);
 
-            var firstName = _station.Tracks.FirstOrDefault()?.Name;
-            _station.DefaultTrackLeft.ReplaceAllValues(btn.Tag.Name, firstName);
-            _station.DefaultTrackRight.ReplaceAllValues(btn.Tag.Name, firstName);
+            var firstName = Tracks.FirstOrDefault()?.Name;
+            DefaultTrackLeft.ReplaceAllValues(btn.Tag.Name, firstName);
+            DefaultTrackRight.ReplaceAllValues(btn.Tag.Name, firstName);
             
             trackRemoves.Add(btn.Tag.Name);
 