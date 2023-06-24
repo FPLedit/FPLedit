392ce1c9d75c2a5432929f26f3f253203a879747
Manuel Huber <info@manuelhu.de>
2023-06-24 13:48:01 +0200

    gtfs: Add basic extension structure

---
+++ FPLedit.GTFS/Model/GtfsAttrs.cs
@@ -0,0 +1,48 @@
+﻿using FPLedit.Shared;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace FPLedit.GTFS.Model
+{
+    [XElmName("gtfs_attrs", IsFpleditElement = true)]
+    public sealed class GTFSAttrs : Entity, IPatternSource
+    {
+        [XAttrName("tp")]
+        public string TrainPatterns
+        {
+            get => GetAttribute("tp", "");
+            set => SetAttribute("tp", value);
+        }
+
+        [XAttrName("sp")]
+        public string StationPatterns
+        {
+            get => GetAttribute("sp", "");
+            set => SetAttribute("sp", value);
+        }
+
+        private GTFSAttrs(Timetable tt) : base("gtfs_attrs", tt)
+        {
+        }
+
+        private GTFSAttrs(XMLEntity en, Timetable tt) : base(en, tt)
+        {
+        }
+
+        public static GTFSAttrs GetAttrs(Timetable tt)
+        {
+            var attrsEn = tt.Children.FirstOrDefault(x => x.XName == "gtfs_attrs");
+            if (attrsEn != null)
+                return new GTFSAttrs(attrsEn, tt);
+            return null;
+        }
+
+        public static GTFSAttrs CreateAttrs(Timetable tt)
+        {
+            var attrs = new GTFSAttrs(tt);
+            tt.Children.Add(attrs.XMLEntity);
+            return attrs;
+        }
+    }
+}
---
+++ FPLedit.GTFS/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using FPLedit.GTFS.Forms;
+using FPLedit.GTFS.Model;
+using FPLedit.Shared;
+using FPLedit.Shared.DefaultImplementations;
+
+namespace FPLedit.GTFS
+{
+    [Plugin("Modul für GTFS-Export", Vi.PFrom, Vi.PUpTo, Author = "Manuel Huber")]
+    public sealed class Plugin : IPlugin
+    {
+        public void Init(IPluginInterface pluginInterface, IComponentRegistry componentRegistry)
+        {
+            //componentRegistry.Register<IAppearanceControl>(new DefaultAppearanceControl(pi => new SettingsControl(pi), T._("GTFS")));
+            componentRegistry.Register<IFilterRuleContainer>(FilterRuleContainer);
+        }
+
+        internal static IFilterRuleContainer FilterRuleContainer => new DefaultFilterRuleContainer(T._("GTFS"), GTFSAttrs.GetAttrs, GTFSAttrs.CreateAttrs);
+    }
+}
